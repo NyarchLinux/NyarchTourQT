2d264194fd175ba760f8e505f0944ada1662a498
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2026-03-11 21:53:59 +0100

    Delete build directory

--- build/lib/nyarchtourqt/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-# SPDX-FileCopyrightText: 2024 Nyarch Linux
-
-"""Nyarch Tour QT - A KDE Kirigami tour application for Nyarch Linux"""
-
-__all__ = ["main"]
-__version__ = "0.1.0"
-
-from .app import main  # noqa: F401
