3885500df12d269eebc2d859ba941ed3fd7fd6f6
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2026-03-18 13:22:24 +0100

    Update for KDe

--- src/nyarchtourqt/pages_model.py
+++ src/nyarchtourqt/pages_model.py
@@ -32,7 +32,7 @@ QML_IMPORT_MAJOR_VERSION = 1
 _RAW_PAGES = [
     {
         "icon": "nyarch-logo",
-        "title": "Welcome to Nyarch",
+        "title": "Welcome to Nyarch Plasma",
         "body": "Nyarch Linux is an Arch Linux-based OS that aims to create the best possible experience for weebs.",
         "buttons": [
             {
@@ -52,39 +52,7 @@ _RAW_PAGES = [
                 "label": "Open Settings",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "gnome-control-center background"
-            }
-        ]
-    },
-    {
-        "icon": "nyarchcustomize-screenshots",
-        "title": "Nyarch Customize",
-        "body": "Change your layout quickly and customize animations and appearance.",
-        "buttons": [
-            {
-                "label": "Open Nyarch Customize",
-                "icon": None,
-                "style": "suggested-action",
-                "command": "flatpak run moe.nyarchlinux.customize"
-            }
-        ]
-    },
-    {
-        "icon": "tweaks-screenshots",
-        "title": "Need advanced customization?",
-        "body": "You can further customize your operating system by editing configurations in Gnome Tweaks and installing new extensions in the Extension Manager.",
-        "buttons": [
-            {
-                "label": "Open Tweaks",
-                "icon": None,
-                "style": "suggested-action",
-                "command": "gnome-tweaks"
-            },
-            {
-                "label": "Open Extension Manager",
-                "icon": None,
-                "style": None,
-                "command": "extension-manager"
+                "command": "systemsettings kcm_wallpaper"
             }
         ]
     },
@@ -97,20 +65,7 @@ _RAW_PAGES = [
                 "label": "Open Catgirl Downloader",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "flatpak run moe.nyarchlinux.catgirldownloader"
-            }
-        ]
-    },
-    {
-        "icon": "waifudownloader-screenshots",
-        "title": "Waifu Downloader",
-        "body": "This application satisfies one of the most important needs a weeb has: getting random pictures of cute anime girls whenever you want!",
-        "buttons": [
-            {
-                "label": "Open Waifu Downloader",
-                "icon": None,
-                "style": "suggested-action",
-                "command": "flatpak run moe.nyarchlinux.waifudownloader"
+                "command": "catgirldownloaderqt"
             }
         ]
     },
@@ -134,91 +89,15 @@ _RAW_PAGES = [
         ]
     },
     {
-        "icon": "shortwave-screenshots",
-        "title": "Listen to your favourite weeb radio with Shortwave",
-        "body": "Shortwave is an Internet radio player that provides access to a station database with over 30,000 stations.",
-        "buttons": [
-            {
-                "label": "Project page",
-                "icon": None,
-                "style": None,
-                "command": "xdg-open https://gitlab.gnome.org/World/Shortwave"
-            },
-            {
-                "label": "Open Shortwave",
-                "icon": None,
-                "style": "suggested-action",
-                "command": "shortwave"
-            }
-        ]
-    },
-    {
-        "icon": "fragments-screenshots",
-        "title": "Download Torrents with Fragments",
-        "body": "Fragments is an easy-to-use BitTorrent client. It can be used to transfer files via the BitTorrent protocol.",
-        "buttons": [
-            {
-                "label": "Project page",
-                "icon": None,
-                "style": None,
-                "command": "xdg-open https://gitlab.gnome.org/World/Fragments"
-            },
-            {
-                "label": "Open Fragments",
-                "icon": None,
-                "style": "suggested-action",
-                "command": "fragments"
-            }
-        ]
-    },
-    {
-        "icon": "lollypop-screenshots",
-        "title": "Listen to music with Lollypop",
-        "body": "Lollypop is an amazingly lightweight music player with a party mode for streaming music from the Internet or playing music from your own collection.",
-        "buttons": [
-            {
-                "label": "Project page",
-                "icon": None,
-                "style": None,
-                "command": "xdg-open https://wiki.gnome.org/Apps/Lollypop"
-            },
-            {
-                "label": "Open Lollypop",
-                "icon": None,
-                "style": "suggested-action",
-                "command": "lollypop"
-            }
-        ]
-    },
-    {
-        "icon": "webapps-screenshots",
-        "title": "Turn your favorite (streaming) websites into apps",
-        "body": "With the Webapp Manager, you can create applications from websites that integrate into your desktop. It supports extensions and many browsers.",
-        "buttons": [
-            {
-                "label": "Project page",
-                "icon": None,
-                "style": None,
-                "command": "xdg-open https://github.com/linuxmint/webapp-manager"
-            },
-            {
-                "label": "Open Webapp Manager",
-                "icon": None,
-                "style": "suggested-action",
-                "command": "webapp-manager"
-            }
-        ]
-    },
-    {
         "icon": "software-screenshots",
         "title": "Need other apps?",
-        "body": "Download your favorite applications from an extensive catalog of applications with Gnome Software.",
+        "body": "Download your favorite applications from an extensive catalog of applications with Discover.",
         "buttons": [
             {
-                "label": "Open Gnome Software",
+                "label": "Open Discover",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "gnome-software"
+                "command": "plasma-discover"
             }
         ]
     },
@@ -231,20 +110,20 @@ _RAW_PAGES = [
                 "label": "Open Nyarch Wizard",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "flatpak run moe.nyarchlinux.wizard"
+                "command": "nyarchwizardqt"
             }
         ]
     },
     {
         "icon": "terminal-screenshots",
         "title": "I use Nyarch, btw",
-        "body": "We have included nyaofetch to let everyone know that you are using Nyarch Linux, and nyaura to download packages from the Arch User Repository and Arch Linux repositories.",
+        "body": "We have included nyaofetch to let everyone know that you are using Nyarch Linux, and nyay to download packages from the Arch User Repository and Arch Linux repositories.",
         "buttons": [
             {
                 "label": "Run in terminal",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "kitty nekofetch"
+                "command": "konsole -e nekofetch"
             }
         ]
     },
@@ -257,7 +136,7 @@ _RAW_PAGES = [
                 "label": "Run Nyarch Scripts",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "flatpak run moe.nyarchlinux.scripts"
+                "command": "nyarchscriptqt"
             }
         ]
     },
@@ -270,7 +149,7 @@ _RAW_PAGES = [
                 "label": "Run Nyarch Updater",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "flatpak run moe.nyarchlinux.updater"
+                "command": "nyarchupdaterqt"
             }
         ]
     },
@@ -283,7 +162,7 @@ _RAW_PAGES = [
                 "label": "Run Nyarch Assistant",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "flatpak run moe.nyarchlinux.assistant"
+                "command": "nyarchassistant"
             }
         ]
     },
@@ -301,28 +180,15 @@ _RAW_PAGES = [
         ]
     },
     {
-        "icon": "online-account-screenshots",
-        "title": "Stay synced",
-        "body": "Add your online accounts in the settings to synchronize calendars, contacts, files and emails.",
-        "buttons": [
-            {
-                "label": "Open Settings",
-                "icon": None,
-                "style": "suggested-action",
-                "command": "gnome-control-center online-accounts"
-            }
-        ]
-    },
-    {
         "icon": "gnome-screenshot",
         "title": "Discover more about your desktop",
-        "body": "Run Gnome Tour to learn more about desktop navigation and touchpad gestures.",
+        "body": "Run Plasma Welcome to learn more about desktop customization",
         "buttons": [
             {
                 "label": "Start Tour",
                 "icon": None,
                 "style": "suggested-action",
-                "command": "gnome-tour"
+                "command": "plasma-welcome"
             }
         ]
     },
